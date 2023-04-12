9ef8275dc75a344f15ab0535ac542f86ce6f1caa
Victor Gianotto <Gianotto@users.noreply.github.com>
2023-04-12 10:12:17 -0300

    Gathering Elos based on SteamID

--- teamsorter.py
+++ teamsorter.py
@@ -1,7 +1,7 @@
-import sys
+import sys, requests
+from valve.steam.id import SteamID
 from itertools import combinations
 
-last_best = 100000000000
 MAX_PLAYERS = 4
 VARIANCE_ACCEPTED = 20
 
@@ -23,6 +23,15 @@ players = {"sn00per": 734,
                 "3Ti-HyperX": 503,
                 "n00t!": 451}
 
+sid_list = {'76561198026118540' : 0,
+            '76561198043250656' : 0,
+            '76561198340444680' : 0,
+            '76561197971071169' : 0,
+            '76561197961495830' : 0,
+            '76561198052156695' : 0,
+            '76561198257497456' : 0,
+            '76561198001099451' : 0}
+
 def unique_group(iterable, k, n, groups=0):
     if groups == k:
         yield []
@@ -31,28 +40,57 @@ def unique_group(iterable, k, n, groups=0):
         for rest in unique_group(pool.difference(combination), k, n, groups + 1):
             yield [combination, *rest]
 
-def variance(groups):
-    total_skills = [sum(players[player] for player in group) for group in groups]
+def variance(groups, plist):
+    total_skills = [sum(plist[player] for player in group) for group in groups]
     return max(total_skills) - min(total_skills)
 
-def team_avg(team):
-    return sum([players[player] for player in team]) / len(team)
+def team_avg(team, plist):
+    return sum([plist[player] for player in team]) / len(team)
 
-i = 0
-for grouping in unique_group(players, len(players)/MAX_PLAYERS, 4):
-    i += 1
-    if i % 100000 == 0:
-        sys.stdout.write('.')
-        sys.stdout.flush()
-    if variance(grouping) < last_best:
-        last_best = variance(grouping)
-        print("")
-        print(f"Variance: {variance(grouping)}")
-        print("============")
-        #print(grouping)
-        
-        for i, time in enumerate(grouping):
-            print("Time {}: {}. Elo: {}".format(i+1, time, team_avg(time)))
-    if last_best <= VARIANCE_ACCEPTED:
-        print("Found best result.")
-        break
+#return elo, sames stats
+def fetch_elo(sid):
+    url = "https://qlstats.net/elo/{}".format(sid)
+    res = requests.get(url)
+    if res.status_code != requests.codes.ok:
+        return 0
+    js = res.json()
+
+    for p in js['players']:
+        _sid = int(p['steamid'])
+        if 'ca' in p: return p['ca']['elo'] #, p['ca']['games']
+        # If the gametype was not found
+        else: return 0
+
+def balance(player_list, num_players_team):
+    last_best = 100000000000
+    i = 0
+    for grouping in unique_group(player_list, len(player_list)/num_players_team, num_players_team):
+        i += 1
+        if i % 100000 == 0:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+        if variance(grouping, player_list) < last_best:
+            last_best = variance(grouping, player_list)
+            print("")
+            print(f"Variance: {variance(grouping, player_list)}")
+            print("============")
+            #print(grouping)
+            
+            for i, time in enumerate(grouping):
+                print("Time {}: {}. Elo: {}".format(i+1, time, team_avg(time, player_list)))
+        if last_best <= VARIANCE_ACCEPTED:
+            print("Found best result.")
+            break
+
+#search for players elos based on steamid
+#build player list
+for sid in sid_list:
+    elo = fetch_elo(sid)
+    print("Steam ID: {} Elo: {}".format(sid, elo))
+    print(SteamID(sid))
+    sid_list[sid] = elo
+
+print([sid_list])
+
+balance(sid_list, MAX_PLAYERS)
+
