93142ec808d9fe211820d2cd13bb06e67b8acfea
Victor Gianotto <Gianotto@users.noreply.github.com>
2023-04-13 17:17:26 -0300

    Fine tunning.

--- teamsorter.py
+++ teamsorter.py
@@ -1,13 +1,16 @@
-import sys, requests
+import sys, requests, time
 from itertools import combinations
 
-MAX_PLAYERS = 4
-VARIANCE_ACCEPTED = 5
+MAX_PLAYERS = 5
+NUM_TEAMS = 6
+VARIANCE_ACCEPTED = 20
+RUNNING_TIME = 5*60
+QLSTATS_URL = 'https://qlstats.net/elo/{}'
 STEAM_KEY = '' # get one at https://steamcommunity.com/dev
 STEAM_API_LINK = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={}'.format(STEAM_KEY)
 STEAM_API_UID = STEAM_API_LINK + '&steamids={}'
-NAME = 0
-ELO = 1
+NAME_KEY = 0
+ELO_KEY = 1
 
 sid_players = { 
                 '76561197985502667' : ['x', 1],
@@ -16,16 +19,35 @@ sid_players = {
                 '76561198340444680' : ['x', 1],
                 '76561197971071169' : ['x', 1],
                 '76561197961495830' : ['x', 1],
-                '76561198257497456' : ['x', 1],
                 '76561198052156695' : ['x', 1],
                 '76561197979265380' : ['x', 1],
+                '76561198257497456' : ['x', 1],
                 '76561198035810278' : ['x', 1],
                 '76561198044624809' : ['x', 1],
-                '76561198001099451' : ['x', 1]}#,
-                #'7' : ['x', 1],
-                #'7' : ['x', 1],
-                #'7' : ['x', 1],
-                #'7' : ['x', 1],}
+                '76561198001099451' : ['x', 1],
+                '76561198242559062' : ['x', 1],
+                '76561198048262475' : ['x', 1],
+                '76561198209854275' : ['x', 1],
+                '76561198154953404' : ['x', 1],
+                '76561198054894320' : ['x', 1],
+                '76561198350007415' : ['x', 1],
+                '76561197993190504' : ['x', 1],
+                '76561198003765572' : ['x', 1],
+                '76561198154996520' : ['x', 1],
+                '76561198315999232' : ['x', 1],
+                '76561198383687171' : ['x', 1],
+                '76561198075370176' : ['x', 1],
+                '76561198405178407' : ['x', 1],
+                '76561198075370176' : ['x', 1],
+                '76561198020698841' : ['x', 1],
+                '76561198120362672' : ['x', 1],
+                '76561198017151203' : ['x', 1],
+                '76561198221203272' : ['x', 1],
+                '76561198198545569' : ['x', 1],
+                '76561199409120459' : ['x', 1],
+                '76561198044013818' : ['x', 1],
+                '76561198118398346' : ['x', 1],
+                '76561197987529787' : ['x', 1]}
 
 
 def unique_group(iterable, k, n, groups=0):
@@ -37,36 +59,70 @@ def unique_group(iterable, k, n, groups=0):
             yield [combination, *rest]
 
 def variance(groups, plist):
-    total_skills = [sum(plist[player][ELO] for player in group) for group in groups]
+    total_skills = [sum(plist[player][ELO_KEY] for player in group) for group in groups]
     return max(total_skills) - min(total_skills)
 
 def team_avg(team, plist):
-    return sum([plist[player][ELO] for player in team]) / len(team)
+    return sum([plist[player][ELO_KEY] for player in team]) / len(team)
 
-def balance(player_list, num_players_team):
+def balance(player_list, num_players_team, num_teams):
+    if len(sid_players) % NUM_TEAMS != 0:
+        print("Found {} players in list.\nNumber of players {} per team must match exact distribution to the amount of desired of {} teams.".format(len(player_list), MAX_PLAYERS, NUM_TEAMS))
+        #return
+    
+    # search for players elos based on steamid
+    # build player list
+    print("Found total of {} players.".format(len(sid_players)))
+    print("Gathering ELO from QLStats.net...")
+    for sid in sid_players:
+        sid_players[sid].pop(ELO_KEY)
+        sid_players[sid].insert(ELO_KEY, fetch_elo(sid))
+        sid_players[sid].pop(NAME_KEY)
+        sid_players[sid].insert(NAME_KEY, getsteam_profile(sid))
+        print("Player: {} ({}) : {}".format(sid_players[sid][NAME_KEY], sid, sid_players[sid][ELO_KEY]))
+
+    print("Shuffling teams based on Elo stats...")
+    initial_time = time.time()
     last_best = 100000000000
     i = 0
-    for grouping in unique_group(player_list, len(player_list)/num_players_team, num_players_team):
+    r = 0
+    for grouping in unique_group(player_list, num_teams, num_players_team):
         i += 1
-        if i % 10000 == 0:
+        if i % 100000 == 0:
             sys.stdout.write('.')
             sys.stdout.flush()
+
+        if time.time() - initial_time > RUNNING_TIME:
+            sys.stdout.flush()
+            print(f"Reach maximum runnnning time. Ended with variance of {last_best}")
+            break
+
         if variance(grouping, player_list) < last_best:
+            r += 1
             last_best = variance(grouping, player_list)
             print("")
-            print(f"Variance: {last_best}")
+            print(f"Round #{r}.\nPlayers variance on same team: {last_best}")
             print("============")
             #print(grouping)
             
-            for i, time in enumerate(grouping):
-                print("Time {}: (Elo: {}). {}".format(i+1, team_avg(time, player_list), [player_list[player][NAME] for player in time]))
+            team_avg_list = list()
+            for i, team in enumerate(grouping):
+                avg = team_avg(team, player_list)
+                team_avg_list.append(avg)
+                print("Time {}: (Elo: {:.0f}). {}".format(i+1, avg, [player_list[player][NAME_KEY] for player in team]))
+
+            print("Teams variance {:.0f}.".format(sum(a for a in team_avg_list)/NUM_TEAMS))
+            print("============")
+
         if last_best <= VARIANCE_ACCEPTED:
             print("Found best result.")
             break
+        
+        
 
-#return elo, sames stats
+# return elo, games stats
 def fetch_elo(sid):
-    url = "https://qlstats.net/elo/{}".format(sid)
+    url = QLSTATS_URL.format(sid)
     res = requests.get(url)
     if res.status_code != requests.codes.ok:
         return 0
@@ -78,22 +134,12 @@ def fetch_elo(sid):
         # If the gametype was not found
         else: return 0
 
-def getSteamProfile(uid):
+def getsteam_profile(uid):
     url = STEAM_API_UID.format(uid)
     res = requests.get(url)
     if res.status_code != requests.codes.ok:
         return 0
     js = res.json()
     return(js['response']['players'][0]['personaname'])
-             
-#search for players elos based on steamid
-#build player list
-for sid in sid_players:
-    sid_players[sid].pop(ELO)
-    sid_players[sid].insert(ELO, fetch_elo(sid))
-    sid_players[sid].pop(NAME)
-    sid_players[sid].insert(NAME, getSteamProfile(sid))
-    print("Player: {} ({}) : {}".format(sid_players[sid][NAME], sid, sid_players[sid][ELO]))
 
-
-balance(sid_players, MAX_PLAYERS)
+balance(sid_players, MAX_PLAYERS, NUM_TEAMS)
